e22a010f911b2b87f23e0b48b166bdc365af142e
Rasmit Devkota <rdatch101@gmail.com>
2025-09-20 19:32:13 -0400

    fixed measurements_to_lattice to use amplitudes instead of probabilities

--- python/analysis.py
+++ python/analysis.py
@@ -31,7 +31,7 @@ def measurements_to_lattice(
 			lattice_direction_bin = "".join(outcome[l] for l in direction_qubits)#[::-1]
 			mu = int(lattice_direction_bin, 2)
 			
-			lattice[*lattice_point, mu] += count/shots
+			lattice[*lattice_point, mu] += np.sqrt(count/shots)
 			
 			if verbose:
 				print(f"Counts for outcome {outcome} at point {lattice_point_bin} ({lattice_point}), in direction {mu} ({lattice_direction_bin}): {count}")

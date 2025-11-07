f0cddcd336bac0c7eb22cf21608967ec4852be9a
RasmitDevkota <rdatch101@gmail.com>
2025-11-07 04:07:33 -0500

    preparation for joss submission

---
+++ docs/source/conf.py
@@ -0,0 +1,114 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "QuaRT-LBM"
+copyright = "2025, Rasmit Devkota and John Wise"
+author = "Rasmit Devkota and John Wise"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ["_templates"]
+
+source_suffix = ".rst"
+
+exclude_patterns = [
+    ".ipynb",
+    "**/**.ipynb_checkpoints",
+    "**/*-checkpoint/",
+    "*-checkpoint/",
+    "**-checkpoint/",
+    "**/**-checkpoint/",
+    "../../QuaRT/**/.ipynb_checkpoints/"
+]
+
+# -- Options for Sphinx AutoAPI ----------------------------------------------
+
+autoapi_dirs = [
+    "../../QuaRT-LBM/",
+]
+
+autoapi_ignore = [
+    "qiskit"
+    "qiskit/"
+    "**/qiskit"
+    "qiskit/**"
+    "**/qiskit/**"
+]
+
+autodoc_inherit_docstrings = False
+
+autoapi_python_class_content = "class"
+
+autodoc_typehints = 'description'
+
+import inspect
+from qiskit.circuit import QuantumCircuit # We need this to check its module path
+
+# Get the base module path for Qiskit to use for comparison
+QISKIT_MODULE_PATH = inspect.getmodule(QuantumCircuit).__name__.split('.')[0]
+print(QISKIT_MODULE_PATH)
+
+def autoapi_skip_member(app, what, name, obj, skip, options):
+    """
+    Skip all members inherited from a class whose defining module is 'qiskit'.
+    """
+
+    print(what, name, obj)
+    print(dir(obj))
+    try:
+        print(obj.docstring)
+    except:
+        pass
+    try:
+        print(obj._docstring)
+    except:
+        pass
+    try:
+        print(obj.docstring_resolved)
+    except:
+        pass
+    
+    # 1. Check if the object is an inherited member
+    if obj.inherited:
+        try:
+            # 2. Use introspection to find the object's origin
+            # obj.qualname is the fully qualified name (e.g., LogicalCircuit.draw)
+            # We get the actual Python object that defines this member.
+            defining_module = inspect.getmodule(obj.obj).__name__
+            
+            # 3. Compare the defining module's root against 'qiskit'
+            if defining_module.startswith(QISKIT_MODULE_PATH):
+                # Skip the member if it originated in Qiskit
+                return True
+            else:
+                print(defining_module)
+                
+        except (AttributeError, TypeError):
+            # If introspection fails for some members (e.g., due to static analysis)
+            # fall back to the default behavior.
+            pass
+
+    # Don't skip if it's not inherited or not from Qiskit
+    return skip
+
+def setup(app):
+    app.connect('autoapi-skip-member', autoapi_skip_member)
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
---
+++ paper/paper.md
@@ -0,0 +1,63 @@
+---
+title: "QuaRT"
+tags:
+  - Python
+  - astronomy
+  - astrophysics
+  - cosmology
+  - radiative transfer
+  - radiation transport
+  - lattice boltzmann methods
+  - computational physics
+  - computational astrophysics
+  - computational cosmology
+  - quantum
+  - quantum information
+  - quantum computing
+  - quantum computation
+  - quantum computer
+  - quantum algorithms
+  - quantum simulation
+  - qiskit
+  - physics
+authors:
+  - name: Rasmit Devkota
+    orcid: 0009-0009-3294-638X
+    affiliation: "1,2"
+  - name: John Wise
+    orcid: 0000-0003-1173-8847
+    affiliation: "1,3"
+affiliations:
+ - name: School of Physics, College of Sciences, Georgia Institute of Technology, Atlanta, GA
+   index: 1
+ - name: School of Mathematics, College of Science, Georgia Institute of Technology, Atlanta, GA
+   index: 2
+ - name: Center for Relativistic Astrophysics, School of Physics, College of Sciences, Georgia Institute of Technology, Atlanta, GA
+   index: 3
+date: XX November 2025
+bibliography: paper.bib
+---
+
+# Summary
+
+`QuaRT` is a Python library for quantum simulation of radiative transfer in astrophysical and cosmological problems.
+
+The source code for `QuaRT` is available on [GitHub](https://github.com/RasmitDevkota/QuaRT/). It can be installed via `pip` from the [`pypi` index](https://pypi.org/project/TODO/). Its [documentation](https://quart.readthedocs.io/) is hosted publicly.
+
+# Statement of need
+
+
+# Functionality
+
+
+# Scholarly Work
+
+
+# Acknowledgements
+
+This research was supported in part through research cyberinfrastructure resources and services provided by the Partnership for an Advanced Computing Environment (PACE) at the Georgia Institute of Technology, Atlanta, Georgia, USA [@PACE].
+
+# References
+
+
+
